9080d454bb8049a9718ccc0e2b4b1491f86272ca
Đinh Hoài Nam <namhoanq0108@gmail.com>
2024-05-02 21:08:13 +0700

    commit new

---
+++ functions/database.php
@@ -0,0 +1,17 @@
+
+<?php 
+function connect(){
+    $host = "127.0.0.1";
+    $user = "root";
+    $pwd = "root";
+    $db = "t2311e_demo";
+    $conn = new mysqli($host,$user,$pwd,$db);
+    if($conn->connect_error)
+        die("Connect refused!");
+    return $conn;
+}
+
+function query($sql){
+    $conn = connect();
+    return $conn->query($sql);
+}
---
+++ html/nav.php
@@ -0,0 +1,26 @@
+
+<?php
+    require_once("functions/product.php");
+    $categories = categories_all();
+?>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+       <?php foreach($categories as $item):?>
+        <li class="nav-item">
+          <a class="nav-link" href="category.php?id=<?php echo $item["id"]; ?>"><?php echo $item["name"]; ?></a>
+        </li>
+        <?php endforeach;?>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
