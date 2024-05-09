e79305d98fcd55dc6b97e6c47fa4377d94727441
Đinh Hoài Nam <namhoanq0108@gmail.com>
2024-05-09 18:20:45 +0700

    commit 9/5/2024

--- functions/checkout.php
+++ functions/checkout.php
@@ -1,4 +1,5 @@
 <?php
+require_once("database.php");
 
 function order_create($order_info,$products,$cart){
     $customer_name = $order_info["customer_name"];
@@ -12,8 +13,15 @@ function order_create($order_info,$products,$cart){
         values('$customer_name','$tel','$address','$grand_total','$status','$shipping_method','$payment_method)";
     $order_id = $insert_get_id($sql);
     foreach($products as $item){
+        $product_id = $item["id"];
+        $bought_qty = $cart[$product_id];
+        $price = $item["price"];
         //insert product to order_products
-
+            $sql_item = "insert into order_products(order_id,product_id,bought_qty,price)
+            values($order_id,$product_id,$bought_qty,$price)";
+        query($sql_item);
         //update qty products table
+        $sql_product = "update products set qty = qty - $bought_qty where id = $product_id";
+        query($sql_product);
     }
 }
